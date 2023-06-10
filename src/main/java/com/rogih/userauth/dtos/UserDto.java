890f7b380d4dcb8cbe524af8516e1b0262a211ac
HigorSouza <higor.andrade@hotmail.com>
2023-06-10 11:57:06 -0300

    Adicionando validação nas requisições

--- src/main/java/com/rogih/userauth/dtos/UserDto.java
+++ src/main/java/com/rogih/userauth/dtos/UserDto.java
@@ -2,6 +2,9 @@ package com.rogih.userauth.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.UUID;
@@ -19,15 +22,23 @@ public class UserDto {
 
     private UUID userId;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Email
     @JsonView(UserView.RegistrationPost.class)
     private String email;
 
+    @Size(min = 6, max = 20)
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
+    @Size(min = 6, max = 20)
+    @NotBlank(groups = UserView.PasswordPut.class)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
 
@@ -40,6 +51,7 @@ public class UserDto {
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView({UserView.ImagePut.class})
     private String imageUrl;
 }
